9755ce118396740699727ccc149e028297a55b4e
Kyle Shockey <shockey@users.noreply.github.com>
2017-11-15 16:39:18 -0800

    Make error printouts prettier

--- index.js
+++ index.js
@@ -16,13 +16,22 @@ const resultSets = fixtures.reduce((res, fixt) => {
         content: fixt.definition
       }).then(errors => {
         console.log(`  ${adaptor.name} \n\n`.blue)
-        console.log(`type: ${typeof errors}\n`)
-        console.log(util.inspect(errors), '\n\n')
+        console.log(`count: ${Array.isArray(errors) ? errors.length : "(not an array)"}\n`)
+        if(!Array.isArray(errors)) {
+          console.log(errors, "\n\n")
+        }
+        errors.forEach(err => {
+          console.log(`
+CODE:      ${err.code ||err.keyword || "(no error code)"}
+PATH:      ${err.path || err.property || err.dataPath || err.field || "(no path)"}
+MESSAGE:   ${err.message || "(no message)"}`)
+        })
+        console.log('\n\n')
         return errors
       }).then(errors => res[fixt.name][adaptor.name] = errors || [])
     } catch(e) {
       console.log(`  ${adaptor.name} \n\n`.blue)
-      console.log(e)
+      console.log(e.message ? `Failed with error: ${e.message}` : e)
       console.log('\n\n\n\n')
     }
   })
